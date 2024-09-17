516f88fe3d03d98e3fba30029809c6736b7195a4
Choirul Huda <choirulhuda07@gmail.com>
2024-09-17 14:30:54 +0700

    UPDATE: -Menambahkan Edittext untuk masukan nama lengkap pada MainActivity

--- app/src/main/java/com/chuda/learning/MainActivity.java
+++ app/src/main/java/com/chuda/learning/MainActivity.java
@@ -3,12 +3,15 @@ package com.chuda.learning;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText edtFullname;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edtFullname = findViewById(R.id.edt_fullname);
     }
 }
